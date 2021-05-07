5b619e97eb117a28bc35f5af68f684747e57e8b6
roynyaga <nyagaandreroy@gmail.com>
2021-05-07 16:12:29 +0100

    add the post_json_data class method

--- lib/services/handle_request.rb
+++ lib/services/handle_request.rb
@@ -1,3 +1,14 @@
 class HandleRequest 
+  include HTTParty
+  @@uri 'https://recruitbot.trikeapps.com/api/v1/roles/bellroy-tech-team-recruit/big_five_profile_submissions'
 
+  @@options = {
+    body: hashed_data.to_json,
+    headers: { "Content-Type" => "application/json"}
+  }
+
+  def self.post_json_data 
+    response = HTTParty.post(@@uri, @@options)
+    puts response 
+  end 
 end 
