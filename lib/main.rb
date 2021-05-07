db062741c0ea88aaa92c91019e5672cb1c5c0efb
roynyaga <nyagaandreroy@gmail.com>
2021-05-07 16:33:44 +0100

    install titleize gem

--- lib/main.rb
+++ lib/main.rb
@@ -3,4 +3,7 @@ require "./services/handle_request.rb"
 
 raw_file_data = File.open("./assets/personality_test_results.txt").readlines.map(&:chomp)
 
-puts raw_file_data
+file_processor = FileProcessor.new(raw_file_data)
+hashed_data = file_processor.create_hash
+
+p hashed_data
