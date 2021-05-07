9c809899adc88f80440073ee7a7131c92694fb67
roynyaga <nyagaandreroy@gmail.com>
2021-05-07 15:51:37 +0100

    add the detect_line_type method

--- lib/services/file_processor.rb
+++ lib/services/file_processor.rb
@@ -25,5 +25,17 @@ class FileProcessor
     return line.split(".").first.gsub(" ","-")
   end
 
+  def detect_line_type(line)
+    last_word = line.split(".").last.strip
+    first_word = line.split(".").first.gsub(" ", "-")
+    if last_word == "Score" then 
+      return "description"
+    elsif @@domains.include?(first_word) then 
+      return "domain"
+    else
+      return "facet"
+    end 
+  end 
+
   
 end
